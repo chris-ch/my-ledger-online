278db7eac7cb45dbd668533d3cdaca0026470dd9
Christophe <ch.alexandre@bluewin.ch>
2018-09-30 17:23:45 +0200

    dev

--- django_container/scripts/load_currencies.py
+++ django_container/scripts/load_currencies.py
@@ -2,5 +2,5 @@ from oas import models
 
 
 def run(*args):
-    print('**PING** {}'.format(str(args)))
-    return models.Currency.objects.all()
+    currencies = models.Currency.objects.all()
+    print('currencies: ' + str(currencies))
--- django_startup/startup.bash
+++ django_startup/startup.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+chmod 755 /startup/*.bash
 
+# Gives more time to Postgres to start: to be replaced with a waiting script
+# as indicated here: https://docs.docker.com/compose/startup-order
+sleep 2
 
 python /startup/manage.py migrate
 
